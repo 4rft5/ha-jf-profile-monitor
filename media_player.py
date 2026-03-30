ce8bad20d3f7739faed885446e6b4f07f911f3de
4rft5 <74219775+4rft5@users.noreply.github.com>
2026-03-30 13:49:10 -0600

    Fixed image SSL bug + warning about async

--- media_player.py
+++ media_player.py
@@ -1,3 +1,4 @@
+import os
 import aiohttp
 from homeassistant.components.media_player import MediaPlayerEntity
 from homeassistant.components.media_player.const import MediaPlayerState
@@ -13,17 +14,23 @@ async def fetch_profiles(api_key, server):
             else:
                 return []
 
+def _write_image(img_path, img_bytes):
+    with open(img_path, "wb") as f:
+        f.write(img_bytes)
+
 class JellyfinProfilePlayer(MediaPlayerEntity):
-    def __init__(self, user_id, username, api_key, server):
+    def __init__(self, user_id, username, api_key, server, hass):
         self._attr_name = f"Jellyfin - {username}"
         self._user_id = user_id
         self._username = username
         self._api_key = api_key
         self._server = server
+        self._hass = hass
         self._state = MediaPlayerState.IDLE
         self._media_title = None
         self._episode_title = None
         self._media_image_url = None
+        self._current_item_id = None
         self._show_title = None
         self._status = "Idle"
         self._icon = "mdi:stop"
@@ -34,13 +41,7 @@ class JellyfinProfilePlayer(MediaPlayerEntity):
                 data = await resp.json()
 
                 if not data:
-                    self._state = MediaPlayerState.IDLE
-                    self._status = "Idle"
-                    self._media_title = None
-                    self._episode_title = None
-                    self._show_title = None
-                    self._media_image_url = None
-                    self._icon = "mdi:stop"
+                    self._set_idle()
                     return
 
                 for session_data in data:
@@ -50,10 +51,25 @@ class JellyfinProfilePlayer(MediaPlayerEntity):
                         self._show_title = item.get("SeriesName")
                         self._episode_title = item.get("Name", "N/A")
                         self._media_title = self._show_title or "Unknown Show"
-                        
-                        tag = item.get("PrimaryImageTag")
-                        id_ = item["Id"]
-                        self._media_image_url = f"{self._server}/Items/{id_}/Images/Primary?tag={tag}"
+
+                        item_id = item["Id"]
+
+                        if item_id != self._current_item_id:
+                            cover_url = f"{self._server}/Items/{item_id}/Images/Primary"
+                            try:
+                                async with aiohttp.ClientSession() as img_session:
+                                    async with img_session.get(cover_url, headers={"X-Emby-Token": self._api_key}) as img_resp:
+                                        if img_resp.status == 200:
+                                            img_bytes = await img_resp.read()
+                                            www_path = self._hass.config.path("www")
+                                            os.makedirs(www_path, exist_ok=True)
+                                            img_filename = f"jellyfin_cover_{self._username}.jpg"
+                                            img_path = os.path.join(www_path, img_filename)
+                                            await self._hass.async_add_executor_job(_write_image, img_path, img_bytes)
+                                            self._current_item_id = item_id
+                                            self._media_image_url = f"/local/{img_filename}?v={item_id}"
+                            except Exception:
+                                self._media_image_url = None
 
                         playback_state = session_data.get("PlayState", {})
                         if playback_state.get("IsPaused", False):
@@ -66,14 +82,17 @@ class JellyfinProfilePlayer(MediaPlayerEntity):
                             self._icon = "mdi:play"
 
                         return
-    
-                self._state = MediaPlayerState.IDLE
-                self._status = "Idle"
-                self._media_title = None
-                self._episode_title = None
-                self._show_title = None
-                self._media_image_url = None
-                self._icon = "mdi:stop"
+
+                self._set_idle()
+
+    def _set_idle(self):
+        self._state = MediaPlayerState.IDLE
+        self._status = "Idle"
+        self._media_title = None
+        self._episode_title = None
+        self._show_title = None
+        self._media_image_url = None
+        self._icon = "mdi:stop"
 
     @property
     def state(self):
@@ -93,7 +112,7 @@ class JellyfinProfilePlayer(MediaPlayerEntity):
             "Show Title": self._show_title,
             "Episode Title": self._episode_title,
         }
-    
+
     @property
     def media_title(self):
         if self._episode_title:
@@ -105,7 +124,7 @@ class JellyfinProfilePlayer(MediaPlayerEntity):
         if self._show_title:
             return self._show_title
         return None
-    
+
     @property
     def media_content_type(self):
         return "music"
@@ -122,6 +141,6 @@ async def async_setup_entry(hass, entry, async_add_entities):
     for profile in profiles:
         user_id = profile["Id"]
         username = profile["Name"]
-        players.append(JellyfinProfilePlayer(user_id, username, api_key, server))
+        players.append(JellyfinProfilePlayer(user_id, username, api_key, server, hass))
 
     async_add_entities(players)
